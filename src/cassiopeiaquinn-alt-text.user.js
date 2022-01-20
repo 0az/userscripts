b7d2be8559445883687d75294db79919f4ca0b4f
0az <0az@afzhou.com>
2022-01-20 16:12:44 -0600

    Remove IIFEs

--- src/cassiopeiaquinn-alt-text.user.js
+++ src/cassiopeiaquinn-alt-text.user.js
@@ -7,47 +7,45 @@
 // @author      -
 // @description 5/4/2021, 11:21:07 AM
 // ==/UserScript==
-;(() => {
-  const img = document.getElementById('cc-comic')
+const img = document.getElementById('cc-comic')
 
-  const alt = document.createElement('div')
-  alt.id = 'alt-text-inject'
-  let text = img.title
-  // Vim Syntax breaks
-  // if (
-  //   new RegExp("^'[^\"]+'$").test(text)
-  // ) {
-  //   let tmp = text.slice(1, -1)
-  //   if (!/'.+'/.test(tmp)) {
-  //     text = `"${tmp}"`
-  //   }
-  // }
-  alt.textContent = text
+const alt = document.createElement('div')
+alt.id = 'alt-text-inject'
+let text = img.title
+// Vim Syntax breaks
+// if (
+//   new RegExp("^'[^\"]+'$").test(text)
+// ) {
+//   let tmp = text.slice(1, -1)
+//   if (!/'.+'/.test(tmp)) {
+//     text = `"${tmp}"`
+//   }
+// }
+alt.textContent = text
 
-  const style = document.createElement('style')
-  style.id = 'alt-text-style-inject'
-  style.textContent = `
-  .cc-nav::after {
-    content: "";
-    display: table;
-    clear: both;
-  }
-  #alt-text-inject {
-    font-size: 16px;
-    margin-top: 1ex;
-  }
-  `
+const style = document.createElement('style')
+style.id = 'alt-text-style-inject'
+style.textContent = /* CSS */ `
+.cc-nav::after {
+  content: "";
+  display: table;
+  clear: both;
+}
+#alt-text-inject {
+  font-size: 16px;
+  margin-top: 1ex;
+}
+`
 
-  const existingAlt = document.getElementById('alt-text-inject')
-  if (existingAlt) {
-    existingAlt.replaceWith(alt)
-  } else {
-    rightside.append(alt)
-  }
-  const existingStyle = document.getElementById('alt-text-style-inject')
-  if (existingStyle) {
-    existingStyle.replaceWith(style)
-  } else {
-    rightside.append(style)
-  }
-})()
+const existingAlt = document.getElementById('alt-text-inject')
+if (existingAlt) {
+  existingAlt.replaceWith(alt)
+} else {
+  rightside.append(alt)
+}
+const existingStyle = document.getElementById('alt-text-style-inject')
+if (existingStyle) {
+  existingStyle.replaceWith(style)
+} else {
+  rightside.append(style)
+}
--- src/webcomic-instant-page.user.js
+++ src/webcomic-instant-page.user.js
@@ -10,40 +10,38 @@
 // @description
 // ==/UserScript==
 
-;(() => {
-  const CACHE = Symbol('webcomic-instant-page-cache')
-  if (!window[CACHE]) {
-    window[CACHE] = new Set()
-  }
-  const prefetches = window[CACHE]
+const CACHE = Symbol('webcomic-instant-page-cache')
+if (!window[CACHE]) {
+  window[CACHE] = new Set()
+}
+const prefetches = window[CACHE]
 
-  function preload(url) {
-    if (prefetches.has(url)) {
-      return
-    }
-
-    const el = document.createElement('link')
-    el.rel = 'prefetch'
-    el.href = url
-    document.head.appendChild(el)
-
-    prefetches.add(url)
+function preload(url) {
+  if (prefetches.has(url)) {
+    return
   }
 
-  const onclickRegex =
-    /(?:window\.)?location\.href\s*=\s*(['"])([^'"]+)(\1)\s*;*/
-  window.requestIdleCallback(() => {
-    Array.from(document.querySelectorAll('a[class*=prev], a[class*=next]'))
-      .map((e) => {
-        if (e.href) {
-          return e.href
-        }
-        if (e.onclick) {
-          return e.getAttribute('onclick').match(onclickRegex)?.[2]
-        }
-      })
-      .forEach((url) => {
-        if (url) preload(url)
-      })
-  })
-})()
+  const el = document.createElement('link')
+  el.rel = 'prefetch'
+  el.href = url
+  document.head.appendChild(el)
+
+  prefetches.add(url)
+}
+
+const onclickRegex =
+  /(?:window\.)?location\.href\s*=\s*(['"])([^'"]+)(\1)\s*;*/
+window.requestIdleCallback(() => {
+  Array.from(document.querySelectorAll('a[class*=prev], a[class*=next]'))
+    .map((e) => {
+      if (e.href) {
+        return e.href
+      }
+      if (e.onclick) {
+        return e.getAttribute('onclick').match(onclickRegex)?.[2]
+      }
+    })
+    .forEach((url) => {
+      if (url) preload(url)
+    })
+})
